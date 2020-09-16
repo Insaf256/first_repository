594edae79100c587ce82a48b3dac046e1ad92c73
Insaf Fayzrakhmanov <insafanas@mail.ru>
2020-09-16 21:47:03 +0300

    update 2.circle

--- 2.circle/Main.java
+++ 2.circle/Main.java
@@ -2,13 +2,12 @@ public class Main {
 	public static void main(String[] args) {
 		if (args.length < 1) {
 			System.out.println("EmptyInput");
-		} else if (Float.valueOf(args[0])  <= 0.0f) {
+		} else if (Doubles.valueOf(args[0])  <= 0.0f) {
 				System.out.println("NegativeRadius");
 		} else {
-			final float Pi = 3.14f;
-			float R = Float.valueOf(args[0]);
-			Float L = 2f * Pi * R;
-			Float S = Pi * R * R / 2;
+			double R = Double.valueOf(args[0]);
+			double L = 2 * Math.PI * R;
+			double S = Math.PI * Math.pow(R, 2) / 2;
 			System.out.println("length: " + L + ";\nSquare: " + S);
 		}
 	}
