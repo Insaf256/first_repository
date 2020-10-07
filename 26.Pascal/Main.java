3f890fecefc3f4fb275566c7372b430ed6b4ec59
Insaf Fayzrakhmanov <insafanas@mail.ru>
2020-10-07 23:23:10 +0300

    Update Packal

--- 26.Pascal/Main.java
+++ 26.Pascal/Main.java
@@ -5,16 +5,25 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		int n = sc.nextInt() - 1;
+
+		int l = 1;
+		for (int i = 1; i <= n - n/2; i++) {
+			l = l * (n/2 + i) / i;
+		}
+		l = (int)(Math.log(l) / Math.log(10)); 
+		l += 2 + l%2;
+		System.out.println(l);
 		for (int i = 0; i <= n; i++) {
 			System.out.print(i + ")\t");
 			int t = 1;
 			for (int j = i; j <= (n*2-1)/2; j++) {
-				System.out.print(" ");
+				for (int k = 0; k < l/2; k++) {
+					System.out.print(" ");
+				}
 			}
 			for (int j = 0; j < i + 1; j++) {
-				System.out.print(t + " ");
-				t *= (i-j);
-				t /= (j+1); 
+				System.out.printf("%" + l + "d", t);
+				t = t * (i-j) / (j+1);
 			}
 			System.out.println();
 		}
